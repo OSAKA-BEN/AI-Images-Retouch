def80b701061ad35fc1fff260974d4e743eb9fe4
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2024-10-20 17:56:12 +0900

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const IBM_Plex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex"
+});
+
+export const metadata: Metadata = {
+  title: "AI Images",
+  description: "An AI powered images retouching tool",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: "#624CF5" }
+    }}>
+      <html lang="en">
+        <body
+          className={cn("font-IBM_Plex antialiased", IBM_Plex.variable)}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
+
